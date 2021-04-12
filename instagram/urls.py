30c38ed62072bdd16d42f42fc5696504d624159c
himanmenGit <study.himanmen@gmail.com>
2021-04-12 11:57:14 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터 8 (03 - JSON 직렬화)

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register('post', views.PostViewSet)
 
 urlpatterns = [
+    path('public/', views.PublicPostListApiView.as_view()),
     path('', include(router.urls))
 ]
