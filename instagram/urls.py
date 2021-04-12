181db7a522929d1fb1e28d733b4e3a5d251d7d7c
himanmenGit <study.himanmen@gmail.com>
2021-04-12 14:41:27 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터 8 (06 - ViewSet과 Router)

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,6 +6,6 @@ router = DefaultRouter()
 router.register('post', views.PostViewSet)
 
 urlpatterns = [
-    path('public/', views.public_post_list),
+    # path('public/', views.public_post_list),
     path('', include(router.urls))
 ]
