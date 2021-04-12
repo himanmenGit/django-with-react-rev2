90362cd3c1e68796039c59633af3f68207790f15
himanmenGit <study.himanmen@gmail.com>
2021-04-12 14:57:30 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터 8 (07 - Renderer를 통한 다양한 응답 포맷 지원)

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,5 +7,6 @@ router.register('post', views.PostViewSet)
 
 urlpatterns = [
     # path('public/', views.public_post_list),
+    path('mypost/<int:pk>/', views.PostDetailAPIView.as_view()),
     path('', include(router.urls))
 ]
