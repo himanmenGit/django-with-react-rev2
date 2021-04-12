6c93d6bd1a1cff233a6cb01873f747abe3ec1532
himanmenGit <study.himanmen@gmail.com>
2021-04-12 11:22:06 +0900

    에듀캐스트 리액트와 함께 장고 시작하기 챕터 8 (02 - JSON 응답뷰 만들기)

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        print("request.body : ", request.body)
+        print("request.body : ", request.POST)
+        return super().dispatch(request, *args, **kwargs)
